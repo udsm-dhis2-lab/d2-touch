5d84960c6bd058bae08b05a171a4e192efe12596
rajey <rajeyw@gmail.com>
2022-06-30 10:01:51 +0300

    add test files for datastore

---
+++ test/data_store_sync_test.dart
@@ -0,0 +1,40 @@
+import 'package:d2_touch/d2_touch.dart';
+import 'package:d2_touch/modules/auth/user/entities/user.entity.dart';
+import 'package:d2_touch/modules/auth/user/queries/user.query.dart';
+import 'package:d2_touch/modules/metadata/dataset/queries/data_set.query.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+import '../sample/current_user.sample.dart';
+import 'data_store_sync_test.reflectable.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences.setMockInitialValues({});
+  initializeReflectable();
+  sqfliteFfiInit();
+
+  var databaseFactory = databaseFactoryFfi;
+
+  await D2Touch.initialize(
+      databaseFactory: databaseFactoryFfi, databaseName: 'flutter_test');
+
+  var db = await databaseFactory.openDatabase(inMemoryDatabasePath);
+
+  UserQuery userQuery = UserQuery(database: db);
+
+  userData['password'] = 'district';
+  userData['isLoggedIn'] = true;
+  userData['username'] = 'admin';
+  userData['baseUrl'] = 'https://play.dhis2.org/2.35.11';
+  final user = User.fromApi(userData);
+  await userQuery.setData(user).save();
+  final dataSetQuery = DataSetQuery(database: db);
+
+  test('should do something', () {
+    expect('', '');
+  });
+}
