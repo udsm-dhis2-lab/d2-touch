02435401f2b0f4d3cf72e0a1d4aa6c7f616046cb
yuzalsif <liteit68@gmail.com>
2023-03-22 12:11:26 -0700

    fix: Add code attribute in userGroup

--- lib/modules/auth/entities/user_group.entity.dart
+++ lib/modules/auth/entities/user_group.entity.dart
@@ -1,26 +1,26 @@
 import 'package:d2_touch/core/annotations/index.dart';
 import 'package:d2_touch/modules/auth/entities/user_group_user.entity.dart';
-import 'package:d2_touch/shared/entities/base.entity.dart';
+import 'package:d2_touch/shared/entities/identifiable.entity.dart';
 
 @AnnotationReflectable
 @Entity(tableName: 'userGroup', apiResourceName: 'userGroups')
-class UserGroup extends BaseEntity {
-  @Column()
-  String name;
+class UserGroup extends IdentifiableEntity{
 
   @OneToMany(table: UserGroupUser)
   List<UserGroupUser>? userGroupUsers;
 
   UserGroup(
       {String? id,
-      required this.name,
+      required name,
       this.userGroupUsers,
+      required String code,
       required bool dirty})
-      : super(id: id, dirty: dirty);
+      : super(id: id, dirty: dirty, code: code, name: name);
 
   factory UserGroup.fromJson(Map<String, dynamic> json) {
     return UserGroup(
       id: json['id'],
+      code: json['code'] ?? '',
       name: json['name'],
       userGroupUsers: json['userGroupUsers'],
       dirty: json['dirty'],
@@ -31,6 +31,7 @@ class UserGroup extends BaseEntity {
     final data = new Map<String, dynamic>();
 
     data['id'] = this.id;
+    data['code'] = this.code;
     data['name'] = this.name;
     data['dirty'] = this.dirty;
     data['userGroupUsers'] = this.userGroupUsers;
