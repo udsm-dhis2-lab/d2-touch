9e571b625fe9715eb893719cf98eb6543b715b57
Bennett Benedict <bennybenester@gmail.com>
2023-03-24 11:24:19 +0300

    Fix error with missing code in usergroup entity

--- lib/modules/auth/entities/user_group.entity.dart
+++ lib/modules/auth/entities/user_group.entity.dart
@@ -1,18 +1,10 @@
 import 'package:d2_touch/core/annotations/index.dart';
 import 'package:d2_touch/modules/auth/entities/user_group_user.entity.dart';
-import 'package:d2_touch/shared/entities/base.entity.dart';
 import 'package:d2_touch/shared/entities/identifiable.entity.dart';
 
 @AnnotationReflectable
 @Entity(tableName: 'userGroup', apiResourceName: 'userGroups')
-class UserGroup extends IdentifiableEntity{
-
-@AnnotationReflectable
-@Entity(tableName: 'userGroup', apiResourceName: 'userGroups')
-class UserGroup extends BaseEntity {
-  @Column()
-  String name;
-
+class UserGroup extends IdentifiableEntity {
   @OneToMany(table: UserGroupUser)
   List<UserGroupUser>? userGroupUsers;
 
