fc74ab16e5b2b7c926bb40ef6838808e9b2b9dfb
Bennett Benedict <bennybenester@gmail.com>
2024-05-16 19:00:53 +0300

    Wrap tracker rule in a try catch block for uncaught errors

--- lib/modules/engine/program_rule/tracker_rule_engine.dart
+++ lib/modules/engine/program_rule/tracker_rule_engine.dart
@@ -20,70 +20,77 @@ class TrackerRuleEngine {
       {required TrackedEntityInstance trackedEntityInstance,
       required String program,
       TrackedEntityAttributeValue? changedAttributeValue}) async {
-    List<ProgramRule> programRules = await ProgramRuleQuery(database: database)
-        .withActions()
-        .where(attribute: 'program', value: program)
-        .get();
+    try {
+      List<ProgramRule> programRules =
+          await ProgramRuleQuery(database: database)
+              .withActions()
+              .where(attribute: 'program', value: program)
+              .get();
 
-    List<ProgramRuleVariable> programRuleVariables =
-        await ProgramRuleVariableQuery(database: database)
-            .where(attribute: 'program', value: program)
-            .get();
+      List<ProgramRuleVariable> programRuleVariables =
+          await ProgramRuleVariableQuery(database: database)
+              .where(attribute: 'program', value: program)
+              .get();
 
-    List<TrackedEntityAttributeValue> attributes =
-        await TrackedEntityAttributeValueQuery(database: database)
-            .where(
-                attribute: 'trackedEntityInstance',
-                value: trackedEntityInstance.trackedEntityInstance)
-            .get();
+      List<TrackedEntityAttributeValue> attributes =
+          await TrackedEntityAttributeValueQuery(database: database)
+              .where(
+                  attribute: 'trackedEntityInstance',
+                  value: trackedEntityInstance.trackedEntityInstance)
+              .get();
 
-    final dataValueEntities = DataValueEntities.fromAttributeValues(attributes);
+      final dataValueEntities =
+          DataValueEntities.fromAttributeValues(attributes);
 
-    List<ProgramRuleAction> programRuleActions = ProgramRuleEngine.execute(
-        dataValueEntities: dataValueEntities,
-        programRules: programRules,
-        programRuleVariables: programRuleVariables);
+      List<ProgramRuleAction> programRuleActions = ProgramRuleEngine.execute(
+          dataValueEntities: dataValueEntities,
+          programRules: programRules,
+          programRuleVariables: programRuleVariables);
 
-    final queue = Queue(parallel: 50);
-    num availableItemCount = 0;
+      final queue = Queue(parallel: 50);
+      num availableItemCount = 0;
 
-    programRuleActions.forEach((programRuleAction) {
-      if (programRuleAction.programRuleActionType == 'ASSIGN' &&
-          programRuleAction.trackedEntityAttribute != null &&
-          programRuleAction.data != null) {
-        availableItemCount++;
-        queue.add(() => TrackedEntityAttributeValueQuery(database: database)
-            .setData(TrackedEntityAttributeValue(
-                synced: false,
-                lastUpdated: DateTime.now().toIso8601String(),
-                id:
-                    "${trackedEntityInstance.trackedEntityInstance}_${programRuleAction.trackedEntityAttribute}",
-                dirty: true,
-                attribute: programRuleAction.trackedEntityAttribute as String,
-                trackedEntityInstance:
-                    trackedEntityInstance.trackedEntityInstance,
-                value: programRuleAction.data as String))
-            .save());
+      programRuleActions.forEach((programRuleAction) {
+        if (programRuleAction.programRuleActionType == 'ASSIGN' &&
+            programRuleAction.trackedEntityAttribute != null &&
+            programRuleAction.data != null) {
+          availableItemCount++;
+          queue.add(() => TrackedEntityAttributeValueQuery(database: database)
+              .setData(TrackedEntityAttributeValue(
+                  synced: false,
+                  lastUpdated: DateTime.now().toIso8601String(),
+                  id:
+                      "${trackedEntityInstance.trackedEntityInstance}_${programRuleAction.trackedEntityAttribute}",
+                  dirty: true,
+                  attribute: programRuleAction.trackedEntityAttribute as String,
+                  trackedEntityInstance:
+                      trackedEntityInstance.trackedEntityInstance,
+                  value: programRuleAction.data as String))
+              .save());
+        }
+      });
+
+      if (availableItemCount == 0) {
+        queue.cancel();
+      } else {
+        await queue.onComplete;
       }
-    });
 
-    if (availableItemCount == 0) {
-      queue.cancel();
-    } else {
-      await queue.onComplete;
+      TrackedEntityInstance? updatedTrackedEntityInstance =
+          await TrackedEntityInstanceQuery(database: database)
+              .withAttributes()
+              .withEnrollments()
+              .byId(trackedEntityInstance.id as String)
+              .getOne();
+
+      return TrackerRuleResult(
+          programRuleActions: programRuleActions,
+          trackedEntityInstance:
+              updatedTrackedEntityInstance ?? trackedEntityInstance);
+    } catch (e) {
+      return TrackerRuleResult(
+          programRuleActions: [], trackedEntityInstance: trackedEntityInstance);
     }
-
-    TrackedEntityInstance? updatedTrackedEntityInstance =
-        await TrackedEntityInstanceQuery(database: database)
-            .withAttributes()
-            .withEnrollments()
-            .byId(trackedEntityInstance.id as String)
-            .getOne();
-
-    return TrackerRuleResult(
-        programRuleActions: programRuleActions,
-        trackedEntityInstance:
-            updatedTrackedEntityInstance ?? trackedEntityInstance);
   }
 
   List<ProgramRuleAction> forEvent(
